e4a1e49a0fca485024c5fe3af138386cd53da530
Matias Sebastian Ramirez Brizuela <ramirezmatias946@gmail.com>
2022-04-02 09:07:53 -0400

    Creamos la presentacion de la lista de usuarios con sus metodos

---
+++ lib/modules/usuarios/presentation/usuario-list/usuario_list_change_notifier.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
+import 'package:test_itti_flutter/modules/usuarios/domain/usuario_entity.dart';
+import 'package:test_itti_flutter/modules/usuarios/infrastructure/dio_usuario_repository.dart';
+import 'package:test_itti_flutter/modules/usuarios/infrastructure/sqlite_usuario_respository.dart';
+
+class UsuarioListChangeNotifier extends ChangeNotifier {
+  List<UsuarioEntity> usuarios = [];
+  final SqliteUsuarioRepository usuarioSqliteRepository =
+      GetIt.I<SqliteUsuarioRepository>();
+  final DioUsuarioRepository usuarioDioRepository =
+      GetIt.I<DioUsuarioRepository>();
+
+  UsuarioListChangeNotifier();
+
+  Future<int> addUsuario(UsuarioEntity usuario) async {
+    notifyListeners();
+    return usuarioSqliteRepository.create(usuario);
+  }
+
+  Future<int> updateUsuario(UsuarioEntity usuario) {
+    notifyListeners();
+    return usuarioSqliteRepository.update(usuario);
+  }
+
+  Future<List<UsuarioEntity>> getAllUsuariosLocal() async {
+    return usuarioSqliteRepository.getAll();
+  }
+
+  Future<List<UsuarioRemoteEntity>> getAllUsuariosRemote() async {
+    return usuarioDioRepository.getAll();
+  }
+
+  Future<UsuarioEntity?> getOneUsuario(String id) async {
+    return usuarioSqliteRepository.getOne(id);
+  }
+
+  Future<void> delete(int id) {
+    notifyListeners();
+    return usuarioSqliteRepository.delete(id);
+  }
+}
---
+++ lib/modules/usuarios/presentation/usuario-list/usuario_list_page.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:test_itti_flutter/modules/usuarios/presentation/usuario-list/widgets/usuario_list_local.dart';
+import 'package:test_itti_flutter/modules/usuarios/presentation/usuario-list/widgets/usuario_list_remote.dart';
+import 'package:test_itti_flutter/modules/usuarios/presentation/usuario-list/widgets/usuario_tab_view.dart';
+
+class UsuarioListPage extends StatelessWidget {
+  const UsuarioListPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 2,
+      initialIndex: 0,
+      child: SafeArea(
+        child: Scaffold(
+          appBar: AppBar(
+            elevation: 0,
+            title: const Text('Usuarios'),
+            leading: SizedBox.fromSize(),
+            centerTitle: true,
+          ),
+          body: Column(
+            children: const [
+              UsuarioTabView(),
+              Expanded(
+                child: TabBarView(
+                  children: [
+                    UsuarioListLocal(),
+                    UsuarioListRemote(),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
