cd8ac42b1d8f61098e09ac63aee1d4fcb7e6440d
Matias Sebastian Ramirez Brizuela <ramirezmatias946@gmail.com>
2022-04-02 09:10:57 -0400

    Dependencias y Widget principal correjimos los multiproviders

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:test_itti_flutter/modules/usuarios/presentation/usuario-form/usuario_form_change_notifier.dart';
+import 'package:test_itti_flutter/modules/usuarios/presentation/usuario-list/usuario_list_change_notifier.dart';
+import 'package:test_itti_flutter/modules/usuarios/presentation/usuario-list/usuario_list_page.dart';
 import 'package:test_itti_flutter/shared/infrastructure/base_sqlite_repository.dart';
+import 'package:test_itti_flutter/shared/infrastructure/get_it_di.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  final sqliteRepository = BaseSqliteRepository();
 
-  runApp(MyApp());
+  setup();
+
+  await initLocalDatabase();
+
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => UsuarioListChangeNotifier(),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => UsuarioFormChangeNotifier(),
+        ),
+      ],
+      child: const MyApp(),
+    ),
+  );
+}
+
+Future<void> initLocalDatabase() async {
+  await BaseSqliteRepository.db.database;
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar'),
-        ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
-          ),
-        ),
-      ),
+    return const MaterialApp(
+      title: 'CRUD Usuarios',
+      home: UsuarioListPage(),
     );
   }
 }
