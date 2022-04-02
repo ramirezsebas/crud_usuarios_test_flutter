7e9d7420b6775580304ab820f1bf367ac1247819
Matias Sebastian Ramirez Brizuela <ramirezmatias946@gmail.com>
2022-04-02 09:05:52 -0400

    Quitamos el repositorio del usuario por que no hay abstraccion para aplicar

--- lib/modules/usuarios/domain/usuario_repository.dart
+++
@@ -1,6 +0,0 @@
-import 'package:test_itti_flutter/modules/usuarios/domain/usuario_entity.dart';
-
-abstract class UsuarioRepository {
-  Future<UsuarioEntity?> getOne(String id);
-  Future<List<UsuarioEntity>> getAll();
-}
