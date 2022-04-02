9124e333490cdf803e2df98a817ba3e8a2e1c349
Matias Sebastian Ramirez Brizuela <ramirezmatias946@gmail.com>
2022-04-02 09:10:27 -0400

    Contenedor para Inyeccion de Dependencia

---
+++ lib/shared/infrastructure/get_it_di.dart
@@ -0,0 +1,14 @@
+import 'package:dio/dio.dart';
+import 'package:get_it/get_it.dart';
+import 'package:test_itti_flutter/modules/usuarios/infrastructure/dio_usuario_repository.dart';
+import 'package:test_itti_flutter/modules/usuarios/infrastructure/sqlite_usuario_respository.dart';
+import 'package:test_itti_flutter/shared/infrastructure/base_sqlite_repository.dart';
+
+final getIt = GetIt.instance;
+
+void setup() {
+  getIt.registerSingleton<BaseSqliteRepository>(BaseSqliteRepository.db);
+  getIt.registerLazySingleton(
+      () => SqliteUsuarioRepository(BaseSqliteRepository.db.instance!));
+  getIt.registerLazySingleton(() => DioUsuarioRepository(Dio()));
+}
