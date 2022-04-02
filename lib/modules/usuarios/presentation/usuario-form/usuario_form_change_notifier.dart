78c5429a9c42d85ea9b45efa795f708619a50b93
Matias Sebastian Ramirez Brizuela <ramirezmatias946@gmail.com>
2022-04-02 09:07:06 -0400

    Agregamos los variables a manejar como estado del form

---
+++ lib/modules/usuarios/presentation/usuario-form/usuario_form_change_notifier.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:test_itti_flutter/modules/usuarios/domain/usuario_entity.dart';
+
+class UsuarioFormChangeNotifier extends ChangeNotifier {
+  final List<String> sexos = ["Femenino", "Masculino"];
+
+  int id = 1;
+  String nombre = "";
+  String selectedSexo = "Femenino";
+  DateTime fechaNacimiento = DateTime.now();
+
+  bool loading = false;
+
+  UsuarioFormChangeNotifier();
+
+  void initUsuario(UsuarioEntity usuarioEntity) {
+    id = usuarioEntity.id ?? 1;
+    nombre = usuarioEntity.nombre;
+    selectedSexo = sexos
+        .where((String element) => element.startsWith(usuarioEntity.sexo))
+        .first;
+    fechaNacimiento = usuarioEntity.fechaNacimiento;
+  }
+
+  void resetUsuario() {
+    nombre = "";
+    selectedSexo = "Femenino";
+    fechaNacimiento = DateTime.now();
+  }
+
+  void changeLoading(bool value) {
+    loading = value;
+    notifyListeners();
+  }
+
+  void changeSexo(String? value) {
+    selectedSexo = value ?? "Femenino";
+    notifyListeners();
+  }
+
+  void changeNombre(String value) {
+    nombre = value;
+    notifyListeners();
+  }
+
+  void changeFechaNacimiento(DateTime value) {
+    fechaNacimiento = value;
+    notifyListeners();
+  }
+
+  String extractFirstLetterSelectedSexo() {
+    return selectedSexo.substring(0, 1);
+  }
+}
